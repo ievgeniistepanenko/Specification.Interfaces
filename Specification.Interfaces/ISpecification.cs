6db0941c4ccf91e4336893c13d1c55f8bc3e1772
Ievgenii Stepanenko <iev@conexio.dk>
2019-07-04 11:09:36 +0200

    add class constraints

--- Specification.Interfaces/ISpecification.cs
+++ Specification.Interfaces/ISpecification.cs
@@ -3,7 +3,7 @@ using System.Linq.Expressions;
 
 namespace Specification.Interfaces
 {
-    public interface ISpecification<T>
+    public interface ISpecification<T> where T:class
     {
         bool IsSatisfiedBy(T entity);
         Expression<Func<T, bool>> ToExpression();
